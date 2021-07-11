7b0c61b9eaa596d5b2191edda0e602907c82b524
Natsuneko <me@mochizuki.moe>
2021-07-12 05:15:15 +0900

    fix: re-support rotation

--- src/operator.py
+++ src/operator.py
@@ -9,7 +9,6 @@ import numpy as np
 
 from typing import List
 from bpy.types import Object, Operator, VectorFont
-from mathutils import Matrix, Vector
 from os import path
 
 from .properties import TextMeshCreatorProperties
@@ -38,17 +37,77 @@ class TextMeshCreatorOperation(Operator):
         finally:
             OperationWrapper.delete_object(context=bpy.context, objects=objects)
 
-    def separate_by_loose_parts(self, object: Object, extrude: float) -> List[Object]:
+    def separate_by_loose_parts(self, object: Object, rotation: List[float], extrude: float) -> List[Object]:
         OperationWrapper.separate_object(context=bpy.context, objects=[object], type="LOOSE")
 
         separated_objects = bpy.context.selected_objects
         objects = []
 
-        for object in separated_objects:
-            center = 0.125 * sum((Vector(bound) for bound in object.bound_box), Vector())
-            origin = object.matrix_world @ center
+        rotation_x = (rotation[0] - math.radians(90)) * -1
+        rotation_y = (rotation[1] - math.radians(0)) * 1
+        rotation_z = (rotation[2] - math.radians(180)) * 1
 
-            if not math.isclose(origin.y, extrude, rel_tol=1e-5):
+        # rotate the base object to align the object's decision axis
+        rot_x = np.array([
+            [1, 0, 0],
+            [0, math.cos(rotation_x), -math.sin(rotation_x)],
+            [0, math.sin(rotation_x), math.cos(rotation_x)]
+        ])
+        rot_y = np.array([
+            [math.cos(rotation_y), 0, -math.sin(rotation_y)],
+            [0, 1, 0],
+            [math.sin(rotation_y), 0, math.cos(rotation_y)]
+        ])
+        rot_z = np.array([
+            [math.cos(rotation_z), -math.sin(rotation_z), 0],
+            [math.sin(rotation_z), math.cos(rotation_z), 0],
+            [0, 0, 1]
+        ])
+
+        # dir vector
+        dec_vector_h = np.array([1, 0, 0])
+        dec_vector_h = rot_x @ dec_vector_h
+        dec_vector_h = rot_y @ dec_vector_h
+        dec_vector_h = rot_z @ dec_vector_h
+        dec_vector_h = dec_vector_h / np.linalg.norm(dec_vector_h)
+
+        # dir vector
+        dec_vector_v = np.array([0, 0, 1])
+        dec_vector_v = rot_x @ dec_vector_v
+        dec_vector_v = rot_y @ dec_vector_v
+        dec_vector_v = rot_z @ dec_vector_v
+        dec_vector_v = dec_vector_v / np.linalg.norm(dec_vector_v)
+
+        # dir vector
+        dec_vector_b = np.array([0, 0, 0])
+
+        # pos vector
+        dec_vector_p = np.array([0, extrude, 0])
+        dec_vector_p = rot_x @ dec_vector_p
+        dec_vector_p = rot_y @ dec_vector_p
+        dec_vector_p = rot_z @ dec_vector_p
+
+        for object in separated_objects:
+            vertices = []
+            for vertex in object.data.vertices:
+                origin = np.array([vertex.co.x, vertex.co.y, vertex.co.z])
+
+                # see: https://manabitimes.jp/math/1197
+                ab = (dec_vector_p + dec_vector_h - origin)
+                ac = (dec_vector_p + dec_vector_v - origin)
+                ad = (dec_vector_p + dec_vector_b - origin)
+
+                matrix = np.array([
+                    [ab[0], ac[0], ad[0]],
+                    [ab[1], ac[1], ad[1]],
+                    [ab[2], ac[2], ad[2]]
+                ])
+
+                r = np.linalg.det(matrix)
+
+                vertices.append(math.isclose(r, 0, abs_tol=1e-5))
+
+            if not all(vertices):
                 OperationWrapper.delete_object(context=bpy.context, objects=[object])
                 continue
 
@@ -58,9 +117,14 @@ class TextMeshCreatorOperation(Operator):
         bpy.ops.object.mode_set(mode="EDIT")
         bpy.ops.mesh.select_mode(type="FACE")
 
+        dec_vector_e = np.array([0, extrude * -2, 0])
+        dec_vector_e = rot_x @ dec_vector_e
+        dec_vector_e = rot_y @ dec_vector_e
+        dec_vector_e = rot_z @ dec_vector_e
+
         OperationWrapper.select_all_in_mesh(context=bpy.context, objects=objects)
         OperationWrapper.extrude_region_move(context=bpy.context, objects=objects,
-                                             transform={"value": [0, -(extrude * 2), 0]})
+                                             transform={"value": dec_vector_e})
         OperationWrapper.select_all_in_mesh(context=bpy.context, objects=objects)
         OperationWrapper.make_normals_consistent(context=bpy.context, objects=objects, inside=False)
 
@@ -108,7 +172,7 @@ class TextMeshCreatorOperation(Operator):
                                             modifier=font_object_f.modifiers[0].name)
 
         if props.separate_by_loose_parts:
-            objects = self.separate_by_loose_parts(font_object_f, props.thickness)
+            objects = self.separate_by_loose_parts(font_object_f, rotation, props.thickness)
             if props.is_preview:
                 return number
             return self.export_object(number, text, props.export_path, objects)
